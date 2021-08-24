3685f031d85babc42d131860765834310be2d72b
Ivan Espinoza <icaro_19@hotmail.com>
2021-08-24 11:23:58 +0200

    get cacalao

--- index.js
+++ index.js
@@ -11,21 +11,24 @@ var rad = Math.PI / 180;
 var frames = 0;
 
 function Particula() {
+  this.r = 50;
   this.x = -this.r;
   this.y = -this.r;
-  this.r = 40;
 
-  this.dibujar = function() {
+  this.dibujar = function () {
     ctx.fillStyle = "#6ab150";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
     ctx.fill();
   }
 
-  this.actualizar = function(momento, frames) { //53, 59, 61, 67 números primos!
+  this.actualizar = function (momento, frames) { //53, 59, 61, 67 números primos!
     this.x = c.x + Math.cos(momento / 23 + Math.cos(momento / 29 + frames * rad)) * (c.x - 2 * this.r);
+    // console.log(this.x)
     this.y = c.y + Math.sin(momento / 31 + Math.cos(momento / 37 + frames * rad)) * (c.y - 2 * this.r);
+    console.log(this.y)
   }
+
 }
 
 particula = new Particula();
@@ -33,11 +36,9 @@ particula.dibujar();
 
 function Animar() {
   elId = window.requestAnimationFrame(Animar);
-  frames += .9;
+  frames += 15;
   ctx.clearRect(0, 0, cw, ch);
   var momento = new Date().getTime() / 30;
-//   console.log(momento)
-//   console.log(new Date().getTime()/30)
   particula.actualizar(momento, frames);
   particula.dibujar();
 
